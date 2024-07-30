fa0f987ba931e02ac383e450a244d227c1b3c0ca
Jessica <jessicaedines15@gmail.com>
2024-07-30 19:25:23 -0300

    ADD TABLE VISTORIA E PRIMEIRA VIEW

--- Esquema.sql
+++ Esquema.sql
@@ -8,7 +8,7 @@ CREATE TABLE Carro (
 )
 CREATE TABLE Oficina(
 
-oficina_id VARCHAR(50) PRIMARY KEY,
+Oficina_id VARCHAR(50) PRIMARY KEY,
 endereço VARCHAR(50) NOT NULL
 nome_oficina VARCHAR(50) NOT NULL
 );
@@ -19,6 +19,18 @@ CREATE TABLE Clientes(
 cpf INT PRIMARY KEY,
 nome VARCHAR(50) NOT NULL
 
-
-
 );
+
+CREATE TABLE Vistoria(
+    Vistoria_id INT PRIMARY KEY,
+    Placa_carro VARCHAR(10),
+    Data_Vistoria DATE,
+    Sinistro BOOLEAN,
+    Oficina_id INT,
+    Nome_responsavel VARCHAR(50),
+    FOREIGN KEY Placa_carro REFERENCES Carro(Placa_carro),
+    FOREIGN KEY Oficina_id REFERENCES Oficina(Oficina_id)
+)
+
+CREATE VIEW Valor_total_do_emprestimo AS 
+SELECT Emprestimos.emprestimo_id, DATEDIFF(Emprestimos.data_inicio,Emprestimos.data_fim) * Carro.Valor_diaria AS Valor_total FROM Emprestimos,Carro WHERE Emprestimos.Placa_carro = Carro.Placa_carro;
