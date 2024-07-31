82875ec0b728b391f4915f63b366d4a63f42a52f
bernardo <bernardo.lima@docente.pr.senac.br>
2024-07-31 20:22:41 -0300

    add create carro

--- MaJeDu_Motors.py
+++ MaJeDu_Motors.py
@@ -1,28 +1,93 @@
+from errno import errorcode
 import mysql.connector
 from mysql.connector import Error
 
-try:
-    
-    connection = mysql.connector.connect(
-        host ='localhost',
-        database = 'majedu_motors',
-        user = 'root',
-        password = 'admin'
-    )
-    
-    if connection.is_connected():
-        db_info = connection.get_server_info()
-        print(f"Connectado ao Servidor MySQL versão {db_info}")
-        cursor = connection.cursor()
-        cursor.execute("SELECT DATABASE();")
-        record = cursor.fetchone()
-        print(f"Você está conectado ao banco de dados: {record}")
-        
-except Error as e:
-    print(f"Erro ao conectar ao MySQL: {e}")
-    
-finally:
-    if connection.is_connected():
+def connect_to_db():
+    try:
+        cnx = mysql.connector.connect(
+            user='root',
+            password='admin',
+            host='127.0.0.1',
+            database='majedu_motors'
+        )
+        return cnx
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Usuário ou senha inválidos")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Banco de dados não encontrado")
+        else:
+            print(err)
+        return None
+
+def execute_query(query, params=None):
+    cnx = connect_to_db()
+    if cnx is None:
+        return
+    cursor = cnx.cursor()
+    try:
+        cursor.execute(query, params)
+        cnx.commit()
+        return cursor
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
         cursor.close()
-        connection.close()
-        print("Conexão ao MySQL foi encerrada")
+        cnx.close()
+
+#Criação de um carro no banco de dados
+def add_car(placa_carro, modelo, cor, ano, marca, valor_diaria):
+    query = ("INSERT INTO Carro (Placa_carro, Modelo, Cor, Ano, Marca, Valor_diaria) "
+             "VALUES (%s, %s, %s, %s, %s, %s)")
+    params = (placa_carro, modelo, cor, ano, marca, valor_diaria)
+    execute_query(query, params)
+
+def main():
+    while True:
+        print("1. Adicionar Carro")
+        print("2. Adicionar Cliente")
+        print("3. Adicionar Oficina")
+        print("4. Adicionar Vistoria")
+        print("5. Novo Emprestimo")
+        print("6. Nova Devolução")
+        print("7. Listar Carros Disponíveis")
+        print("8. Listar Todos os Carros")
+        print("9. Listar Usuários")
+        print("10. Listar Empréstimos, seus valores totais, e os valores das multas associadas a eles")
+        # Adicionar mais opções aqui...
+        print("0. Sair")
+        choice = input("Selecione uma opção: ")
+
+        if choice == '1': #Criar carro
+            placa_carro = input("Digite a placa do carro: ")
+            modelo = input("Digite o modelo: ")
+            cor = input("Digite a cor: ")
+            ano = input("Digite o ano: ")
+            marca = input("Digite a marca: ")
+            valor_diaria = input("Digite o valor da diária: ")
+            add_car(placa_carro, modelo, cor, ano, marca, valor_diaria)
+        elif choice == '2': #Criar cliente
+            pass
+        elif choice == '3': #Criar oficina
+            pass
+        elif choice == '4': #Criar vistoria
+            pass
+        elif choice == '5': #Novo empréstimo
+            pass
+        elif choice == '6': #Nova Devolução
+            pass
+        elif choice == '7': #Listar Carros Disponíveis
+            pass
+        elif choice == '8': #Listar Todos os Carros
+            pass
+        elif choice == '9': #Listar Usuários
+            pass
+        elif choice == '10': #Listar Empréstimos, seus valores totais, e os valores das multas associadas a eles
+            pass
+        elif choice == '0': #Sair
+            break
+        else:
+            print("Escolha inválida!")
+
+if __name__ == "__main__":
+    main()
--- trigger.sql
+++ trigger.sql
@@ -1,4 +1,6 @@
-
+DROP TRIGGER verificar_carro_emprestado;
+DROP TRIGGER check_return_date;
+DROP TRIGGER check_car_inspection;
 
 CREATE TRIGGER verificar_carro_emprestado
 BEFORE INSERT ON Emprestimos
@@ -23,4 +25,45 @@ BEGIN
     END IF;
 END//
 
+DELIMITER //
 
+CREATE TRIGGER check_return_date
+BEFORE INSERT ON emprestimos
+FOR EACH ROW
+BEGIN
+    IF NEW.data_devolucao < NEW.data_emprestimo THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'A data de devolução não pode ser anterior à data de empréstimo.';
+    END IF;
+END//
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER check_car_inspection
+BEFORE INSERT ON emprestimos
+FOR EACH ROW
+BEGIN
+    DECLARE last_devolution DATE;
+    DECLARE last_inspection DATE;
+
+    -- Encontre a última data de devolução para o carro
+    SELECT MAX(data_devolucao) INTO last_devolution
+    FROM emprestimos
+    WHERE Placa_carro = NEW.Placa_carro;
+
+    -- Encontre a última vistoria após a última devolução
+    SELECT MAX(data_vistoria) INTO last_inspection
+    FROM vistorias
+    WHERE Placa_carro = NEW.Placa_carro
+      AND data_vistoria > last_devolution;
+
+    -- Verifique se a última vistoria é depois da última devolução
+    IF last_inspection IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'O carro precisa passar por uma vistoria antes de ser emprestado.';
+    END IF;
+END//
+
+DELIMITER ;
