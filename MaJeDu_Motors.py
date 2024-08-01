15756f9c59cf701d0494fcfaa634a84378f6226e
Jessica <jessicaedines15@gmail.com>
2024-07-31 21:00:38 -0300

    ADD

--- MaJeDu_Motors.py
+++ MaJeDu_Motors.py
@@ -42,6 +42,18 @@ def add_car(placa_carro, modelo, cor, ano, marca, valor_diaria):
     params = (placa_carro, modelo, cor, ano, marca, valor_diaria)
     execute_query(query, params)
 
+def add_cliente(cpf, Nome, Cnh, Rua, Numero_casa, Complemento, Cep, Telefone, Email):
+    query = ("INSERT INTO Clientes(Cpf, Nome, Cnh, Rua, Numero_casa, Complemento, Cep, Telefone, Email)"
+             "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)")
+    params = (cpf, Nome, Cnh, Rua, Numero_casa, Complemento, Cep, Telefone, Email)
+    execute_query(query,params)
+
+def add_vistoria(Placa_carro, Data_Vistoria, Sinistro, Oficina_id, Nome_responsavel, Valor_vistoria):
+    query = ("INSERT INTO Vistoria (Placa_carro, Data_Vistoria, Sinistro, Oficina_id, Nome_responsavel, Valor_vistoria)" 
+             "VALUES(%s, %s, %s, %s, %s, %s)")
+    params =(Placa_carro, Data_Vistoria, Sinistro, Oficina_id, Nome_responsavel, Valor_vistoria) 
+    execute_query(query,params)
+
 def main():
     while True:
         print("1. Adicionar Carro")
@@ -67,11 +79,26 @@ def main():
             valor_diaria = input("Digite o valor da diária: ")
             add_car(placa_carro, modelo, cor, ano, marca, valor_diaria)
         elif choice == '2': #Criar cliente
-            pass
+            cpf = input("Digite o cpf do cliente: ")
+            Nome = input("Digite o nome do cliente: ")
+            Cnh = input("Digite a Cnh: ")
+            Rua = input("Digite a Rua: ")
+            Numero_casa = input("Digite o numero da casa: ")
+            Complemento = input("Digete complemento: ")
+            Cep = input("Digite o CEP: ")
+            Telefone = input("Digite um numero de telefone: ")
+            Email = input("Digite um e-mail: ")
+            add_cliente(cpf, Nome, Cnh, Rua, Numero_casa, Complemento, Cep, Telefone, Email)
         elif choice == '3': #Criar oficina
             pass
         elif choice == '4': #Criar vistoria
-            pass
+            Placa_carro = input("Digite placa do carro: ") 
+            Data_Vistoria = input("Digite data de vistoria: ") 
+            Sinistro = input("Digite 1 se passou ou 0 se não passou pela vistoria: ") 
+            Oficina_id = input("Digite o id da oficina: ") 
+            Nome_responsavel = input("Digite o nome do responsável pela vistoria: ") 
+            Valor_vistoria = input("Digite o valor da vistoria: ") 
+            add_vistoria(Placa_carro, Data_Vistoria, Sinistro, Oficina_id, Nome_responsavel, Valor_vistoria)
         elif choice == '5': #Novo empréstimo
             pass
         elif choice == '6': #Nova Devolução
